ff67dc632412e49b7a5b24d90a224c0e119246ad
razvanFarcas30 <razvan.farcas2002@gmail.com>
2024-01-08 14:31:05 +0200

    Dupa crearea paginilor Razor si lucrat la functionalitati

---
+++ Data/Migrations/20240106202416_InitialCreate.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProiectMediiProgramare.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeCategorie = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorie", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nume = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adresa = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumarTelefon = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Oras",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nume = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Oras", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salon",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nume = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrasID = table.Column<int>(type: "int", nullable: true),
+                    Adresa = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Pret = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategorieID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salon", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Salon_Categorie_CategorieID",
+                        column: x => x.CategorieID,
+                        principalTable: "Categorie",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Salon_Oras_OrasID",
+                        column: x => x.OrasID,
+                        principalTable: "Oras",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Programare",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientID = table.Column<int>(type: "int", nullable: true),
+                    SalonID = table.Column<int>(type: "int", nullable: true),
+                    DataProgramarii = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Programare", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Programare_Client_ClientID",
+                        column: x => x.ClientID,
+                        principalTable: "Client",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Programare_Salon_SalonID",
+                        column: x => x.SalonID,
+                        principalTable: "Salon",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stilist",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nume = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Pret = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SalonID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stilist", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Stilist_Salon_SalonID",
+                        column: x => x.SalonID,
+                        principalTable: "Salon",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Programare_ClientID",
+                table: "Programare",
+                column: "ClientID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Programare_SalonID",
+                table: "Programare",
+                column: "SalonID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salon_CategorieID",
+                table: "Salon",
+                column: "CategorieID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salon_OrasID",
+                table: "Salon",
+                column: "OrasID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stilist_SalonID",
+                table: "Stilist",
+                column: "SalonID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Programare");
+
+            migrationBuilder.DropTable(
+                name: "Stilist");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "Salon");
+
+            migrationBuilder.DropTable(
+                name: "Categorie");
+
+            migrationBuilder.DropTable(
+                name: "Oras");
+        }
+    }
+}
